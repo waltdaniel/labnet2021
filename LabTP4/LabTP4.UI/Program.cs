17cb258c235ac0b84c523902ff822323ff6a2d54
walterd <walterdgomez@yahoo.com.ar>
2021-09-22 17:36:58 -0300

    Mostrar datos básicos Customers

--- LabTP4/LabTP4.UI/Program.cs
+++ LabTP4/LabTP4.UI/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using TP4.Entities;
+using System;
+using TP4.Logic;
 namespace LabTP4.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CustomersLogic customersLogic = new CustomersLogic();
+            foreach(Customers c in customersLogic.GetTodos())
+            {
+                Console.WriteLine(String.Concat("CustomerID: ", c.CustomerID, " Contact name: ", c.ContactName, " Contact Title: ", c.ContactTitle));
+            }
+            Console.Read();
         }
     }
 }
