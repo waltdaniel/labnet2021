c18ada504c3af16c60be4baf0663803933333a28
walterd <walterdgomez@yahoo.com.ar>
2021-09-22 17:06:48 -0300

    Mostrar datos básicos Curstomers

---
+++ LabTP4/TP4.Logic/CustomersLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.Data;
+using TP4.Entities;
+
+namespace TP4.Logic
+{
+    public class CustomersLogic
+    {
+        private readonly EntityContext context;
+        public CustomersLogic()
+        {
+            context = new EntityContext();
+        }
+        public List<Customers> GetTodos()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
